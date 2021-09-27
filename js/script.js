8353eb8d8bcb549e189960cc657e49a55e359404
Gollito <you@example.com>
2021-09-27 12:45:44 -0500

    Actualizacion 27/09

--- js/script.js
+++ js/script.js
@@ -33,3 +33,22 @@ const typed = new Typed('.typed', {
 	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
+const typed2 = new Typed('.typed2', {
+    strings:['<i class="Actividad" > Ingeniero de sistemas</i>',
+      '<i class="Actividad" > Creador de contenido</i>',
+      '<i class="Actividad" > Diseñador web</i>',
+      '<i class="Actividad" > creador de contenido</i>'
+    ],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: true, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
